0a57779cf8a534fef685ded469fd527f588574b4
Shidqi Yusri <shidqifadhlu@gmail.com>
2022-06-15 12:02:53 +0700

    final edit i hope

--- main.py
+++ main.py
@@ -110,49 +110,6 @@ if'__main__'==__name__:
     R2.cmdPrint('ping -c 4 192.168.126.3') # R2 to R3
     R2.cmdPrint('ping -c 4 192.168.116.3') # R2 to R4
 
-    #Routing lain
-    # HostA.cmd('sysctl net.ipv4.ip_forward=1')
-    # HostA.cmd('ip route add 0.0.0.0/0 via')
-
-    # R1.cmd('sysctl net.ipv4.ip_forward=1')
-    # R1.cmd('ip route add 0.0.0.0/0 via 192.168.60.3')
-    # # r1.cmd('ip route add 192.168.1.0/24 via 192.168.255.6 dev R1-eth1')
-    # # r1.cmd('ip route add 192.168.255.8/30 via 192.168.255.6 dev R1-eth1')
-    # # r1.cmd('ip route add 192.168.255.12/30 via 192.168.255.6 dev R1-eth1')
-    # # r1.cmd('ip route add 192.168.2.0/24 via 192.168.255.6 dev R1-eth1')
-    # # r1.cmd('ip route add 192.168.3.0/24 via 192.168.255.6 dev R1-eth1')
-
-    # R2.cmd('sysctl net.ipv4.ip_forward=1')
-    # R2.cmd('ip route add 0.0.0.0/0 via 192.168.63.3')
-    # R2.cmd('ip route add 192.168.59.0/24 via 192.168.63.3')
-    # # r2.cmd('ip route add 192.168.0.0/24 via 192.168.255.10 dev R2-eth1')
-    # # r2.cmd('ip route add 192.168.255.0/30 via 192.168.255.10 dev R2-eth1')
-    # # r2.cmd('ip route add 192.168.255.4/30 via 192.168.255.10 dev R2-eth1')
-    # # r2.cmd('ip route add 192.168.2.0/24 via 192.168.255.10 dev R2-eth1')
-    # # r2.cmd('ip route add 192.168.3.0/24 via 192.168.255.10 dev R2-eth1')
-
-    # R3.cmd('sysctl net.ipv4.ip_forward=1')
-    # # r3.cmd('ip route add 0.0.0.0/0 via 192.168.255.1 dev R3-eth2')
-    # R3.cmd('ip route add 192.168.56.0/24 via 192.168.60.2')
-    # R3.cmd('ip route add 192.168.57.0/24 via 192.168.63.2')
-    # R3.cmd('ip route add 192.168.61.0/24 via 192.168.60.2')
-    # R3.cmd('ip route add 192.168.62.0/24 via 192.168.63.2')
-    # R3.cmd('ip route add 192.168.59.0/24 via 192.168.63.2')
-
-    # # r3.cmd('ip route add 192.168.0.0/24 via 192.168.255.1 dev R3-eth2')
-    # # r3.cmd('ip route add 192.168.1.0/24 via 192.168.255.1 dev R3-eth2')
-    # # r3.cmd('ip route add 192.168.255.4/30 via 192.168.255.1 dev R3-eth2')
-    # # r3.cmd('ip route add 192.168.255.12/30 via 192.168.255.1 dev R3-eth2')
-    # # r3.cmd('ip route add 192.168.3.0/24 via 192.168.255.1 dev R3-eth2')
-
-    # R4.cmd('sysctl net.ipv4.ip_forward=1')
-    # R4.cmd('ip route add 0.0.0.0/0 via 192.168.62.2')
-    # # r4.cmd('ip route add 192.168.0.0/24 via 192.168.255.13 dev R4-eth2')
-    # # r4.cmd('ip route add 192.168.1.0/24 via 192.168.255.13 dev R4-eth2')
-    # # r4.cmd('ip route add 192.168.255.0/30 via 192.168.255.13 dev R4-eth2')
-    # # r4.cmd('ip route add 192.168.255.8/30 via 192.168.255.13 dev R4-eth2')
-    # # r4.cmd('ip route add 192.168.2.0/24 via 192.168.255.13 dev R4-eth2')
-
     #Routing setiap perangkat yang bertetangga
     HostA.cmd("ip rule add from 192.168.56.2 table 1")
     HostA.cmd("ip rule add from 192.168.66.2 table 2")
@@ -235,18 +192,8 @@ if'__main__'==__name__:
     R4.cmd("route add -net 192.168.96.0/24 gw 192.168.106.2")
     R4.cmd("route add -net 192.168.126.0/24 gw 192.168.116.2")
 
-    #Test IP after routing
-    # HostA.cmdPrint('ping -c 4 192.168.56.3')    # A to R1
-    # HostA.cmdPrint('ping -c 4 192.168.57.3')    # A to R2
-    # HostB.cmdPrint('ping -c 4 192.168.58.3')     # B to R3
-    # HostB.cmdPrint('ping -c 4 192.168.59.3')     # B to R4
-    # R1.cmdPrint('ping -c 4 192.168.60.3')  # R1 to R3
-    # R1.cmdPrint('ping -c 4 192.168.61.3')  # R1 to R4
-    # R2.cmdPrint('ping -c 4 192.168.63.3') # R2 to R3
-    # R2.cmdPrint('ping -c 4 192.168.62.3') # R2 to R4
-
-    R1.cmdPrint("tc qdisc del dev R1-eth0 root")
-    R1.cmdPrint("tc qdisc add dev R1-eth0 root netem delay 20ms")
+    # R1.cmdPrint("tc qdisc del dev R1-eth0 root")
+    # R1.cmdPrint("tc qdisc add dev R1-eth0 root netem delay 20ms")
 
     #R2.cmdPrint("tc qdisc del dev R2-eth1 root")
     #R2.cmdPrint("tc qdisc add dev R2-eth1 root netem loss 20%")
@@ -254,13 +201,13 @@ if'__main__'==__name__:
     #R3.cmdPrint("tc qdisc del dev R3-eth0 root")
     #R3.cmdPrint("tc qdisc add dev R3-eth0 root netem delay 40ms")
 
-    time.sleep(2)
-    # run background traffic
-    HostB.cmd("iperf -s &") #Buat server
-    HostB.cmd("tcpdump -w test.pcap &") #Bikin file wireshark
-    HostA.cmd("iperf -c 192.168.76.2 -t 100 &") #Buat client dan BEBAN HIDUP
-    time.sleep(2)
-    HostA.cmd("iperf -c 192.168.76.2") #BEBAN HIDUP
+    # time.sleep(2)
+    # # run background traffic
+    # HostB.cmd("iperf -s &") #Buat server
+    # HostB.cmd("tcpdump -w test.pcap &") #Bikin file wireshark
+    # HostA.cmd("iperf -c 192.168.76.2 -t 100 &") #Buat client dan BEBAN HIDUP
+    # time.sleep(2)
+    # HostA.cmd("iperf -c 192.168.76.2") #BEBAN HIDUP
 
     CLI(net)
 
